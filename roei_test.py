052132196f8ca7faf11ceb78d420d53428478c9f
roeilevinson <roei.levinson@gmail.com>
2024-02-10 00:16:56 +0200

    left and right rotations

--- roei_test.py
+++ roei_test.py
@@ -6,15 +6,18 @@ tree = AVLTree()
 # Constants for random number generation
 MIN_KEY = 1
 MAX_KEY = 1000
-NODES= [5,3,8,9,1,6]
+NODES= [1,2,3,4]
 
 # Generate and insert 50 random nodes
 for i in range(len(NODES)):
     key = NODES[i]
     tree.insert(key, "")
-    tree.print_tree()
-
-
-node = tree.search(8)
+print("****")
+node = tree.get_root().get_right()
+tree.left_rotation(node)
+tree.print_tree()
+print("****")
+node = tree.search(3)
 tree.delete(node)
 tree.print_tree()
+
